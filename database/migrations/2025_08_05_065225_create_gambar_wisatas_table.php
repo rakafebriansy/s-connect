ad7204ab09c047b743e31b48493dc17ab7cc9a2c
rakafebriansy <rakafebrian19@gmail.com>
2025-08-09 17:52:27 +0700

    fix: ondelete cascade gambar wisata

--- database/migrations/2025_08_05_065225_create_gambar_wisatas_table.php
+++ database/migrations/2025_08_05_065225_create_gambar_wisatas_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
     {
         Schema::create('gambar_wisatas', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('wisata_id')->constrained();
+            $table->foreignId('wisata_id')->constrained()->onDelete('cascade');
             $table->string('gambar');
             $table->timestamps();
         });
